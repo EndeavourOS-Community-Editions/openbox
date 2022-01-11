ae515202e17f7b566470ffe0ce65f66cd9d3fa89
Geckobit <Geckobit@users.noreply.github.com>
2022-01-10 19:50:14 -0600

    Updated scrot commands to use internal date format

--- .config/openbox/scripts/screenshot.sh
+++ .config/openbox/scripts/screenshot.sh
@@ -9,11 +9,12 @@ option2="window"
 options="$option0\n$option1\n$option2"
 
 selected="$(echo -e "$options" | rofi -lines 3 -dmenu -p "scrot")"
+filename="Screenshot-%Y-%m-%d_%H:%M:%S.png"
 case $selected in
     $option0)
-        cd ~/Pictures/ && sleep 1 && scrot;;
+        cd ~/Pictures/ && sleep 1 && scrot "$filename";;
     $option1)
-        cd ~/Pictures/ && scrot -s;;
+        cd ~/Pictures/ && scrot -s "$filename";;
     $option2)
-        cd ~/Pictures/ && sleep 1 && scrot -u;;
+        cd ~/Pictures/ && sleep 1 && scrot -u "$filename";;
 esac
